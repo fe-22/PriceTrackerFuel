3ddb665fef26ea73963b28d0b90f820d33c6b515
f22 chechin ADFidelidade <fernandoalexandrefernandes@gmail.com>
2025-11-29 22:07:12 -0300

    fix: connect myapp URLs to display real application

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("""
-        <h1>PriceTrackerFuel está funcionando! 🎉</h1>
-        <p>Deploy no Render realizado com sucesso!</p>
-        <a href="/admin/">Admin</a>
-    """)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('myapp.urls')),  # ← Conecta ao myapp
 ]
