9338326913052dca5e313a10cfd8b77c1871ab17
f22 chechin ADFidelidade <fernandoalexandrefernandes@gmail.com>
2025-11-29 21:54:28 -0300

    fix: add ROOT_URLCONF and complete Django settings

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("""
+        <h1>PriceTrackerFuel está funcionando! 🎉</h1>
+        <p>Deploy no Render realizado com sucesso!</p>
+        <a href="/admin/">Admin</a>
+    """)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')), 
+    path('', home),
 ]
